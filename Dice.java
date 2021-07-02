0ac548e265d103e21ad212db65415f6c40f4147c
ko <s1280081@u-aizu.ac.jp>
2021-07-02 12:32:44 +0900

    Initial revision

---
+++ Dice.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+class Dice{
+    public static void main(String[] args){
+	Random r=new Random();
+	
+	int die1=r.nextInt(6)+1;
+	int die2=r.nextInt(6)+1;
+	System.out.println("Rolling the Dice...");
+	System.out.println("Die 1: "+die1);
+	System.out.println("Die 2: "+die2);
+	int total=die1+die2;
+	System.out.println("Total value: "+total);
+    }
+}
