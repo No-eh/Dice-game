5ee3eb0e4f15ae5d82fd8b2e1aee6605e6a057e8
ko <s1280081@u-aizu.ac.jp>
2021-07-08 12:29:55 +0900

    Won/lost massage added

--- Dice.java
+++ Dice.java
@@ -10,5 +10,9 @@ class Dice{
 	System.out.println("Die 2: "+die2);
 	int total=die1+die2;
 	System.out.println("Total value: "+total);
+	if(total>7) {
+	    System.out.println("You won");
+	}
+	else System.out.println("You lost");
     }
 }
